5bf4433c64017e0d156daf5b5a657a8c17616ffe
Vincent Mogah <mogah.vincent@hotmail.com>
2026-01-19 19:27:19 +0000

    Update application versions: ArgoCD 9.3.4, Promtail 6.17.1, NGINX 4.14.1, Metrics 3.13.0, RocketChat 6.29.0

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -4,7 +4,7 @@ resource "helm_release" "argocd" {
   chart            = "argo-cd"
   namespace        = "argocd"
   create_namespace = true
-  version          = "5.51.6"
+  version          = "9.3.4"
 
   values = [
     <<EOF
